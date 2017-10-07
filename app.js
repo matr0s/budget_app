faeeea1a31937ea0da6a9617027760f5d117f6f0
matr0s <serhiy.osadchuk@gmail.com>
2017-10-07 18:08:58 +0300

    Modules basic structure

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 var budgetController = (function(){
+	var x = 30;
+	var add = function(a) {
+		return x + a;
+		}
 
-var x = 30;
-var add = function(a) {
-	return x + a;
+	return {
+		publicTest: function(b) {
+			return add(b);
+		}
 }
+})();
 
-return {
-	publicTest: function(b) {
-		console.log(add(b));
-	}
-}
+
+var UIController = (function() {
+
+// Some code here
 
 
 })();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+//Some code here
+var z  = budgetCtrl.publicTest(2);
+
+return {
+	anotherTest: function(){
+		console.log(z);
+	}
+}
+
+})(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
+
