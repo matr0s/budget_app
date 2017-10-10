f8ebe619a27215a4cdea67fdac5a2950ababaa5b
Serhii Osadchuk <matros@matross-MacBook-Pro.local>
2017-10-10 18:45:22 +0300

    Get input data

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
-	var x = 30;
-	var add = function(a) {
-		return x + a;
-		}
 
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-}
+// SOME CODE HERE
+
 })();
 
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
-// Some code here
-
+return {
+  getinput: function() {
+      var type = document.querySelector('.add__type').value;
+      var description = document.querySelector('.add__description').value;
+      var value = document.querySelector('.add__value').value;
+  }
+};
 
 })();
 
 
+
+
+// MAIAN APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-//Some code here
-var z  = budgetCtrl.publicTest(2);
+    var ctrlAddItem = function() {
+    // 1. Get data from input
 
-return {
-	anotherTest: function(){
-		console.log(z);
-	}
-}
+    // 2. Add item to the Budget contoller
+
+    // 3. Add itme to UI
+
+    // 4. Calculate the budger
+
+    // 5. Display the budget
+        console.log('test');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+       if (event.keyCode === 13 || event.which === 13) {
+       ctrlAddItem();
+       }
+    });
+
 
 })(budgetController, UIController);
 
