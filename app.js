a016585ef122430c4004af043e9e70281fd040aa
Serhii Osadchuk <matros@matross-MacBook-Pro.local>
2017-12-12 19:35:31 +0200

    Get input data

--- app.js
+++ app.js
@@ -10,14 +10,25 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'  ,
+        inputBtn: '.add__btn'
+    };
+
 return {
   getinput: function() {
-      var type = document.querySelector('.add__type').value;
-      var description = document.querySelector('.add__description').value;
-      var value = document.querySelector('.add__value').value;
-  }
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+            };
+  },
+    getDOMstrings: function() {
+        return DOMstrings;
+    }
 };
-
 })();
 
 
@@ -26,9 +37,12 @@ return {
 // MAIAN APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
     // 1. Get data from input
-
+        var input = UICtrl.getinput();
+        console.log(input);
     // 2. Add item to the Budget contoller
 
     // 3. Add itme to UI
@@ -36,15 +50,17 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budger
 
     // 5. Display the budget
-        console.log('test');
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
+
        if (event.keyCode === 13 || event.which === 13) {
        ctrlAddItem();
        }
+
     });
 
 
